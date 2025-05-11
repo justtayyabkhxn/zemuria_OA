bfb016aaf47ea8a1ef97efbb2d3aab4d8edcb288
Tayyab Khan <tayyabkhangk4734@gmail.com>
2025-05-11 23:23:49 +0530

    fix

--- src/backend/langflow/api/routes/custom_query.py
+++ src/backend/langflow/api/routes/custom_query.py
@@ -19,7 +19,7 @@ async def query_openai(request:QueryRequest):
             llm=ChatOpenAI(
                 temperature=0.7,
                 model_name="gpt-3.5-turbo",
-                openai_api_key="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",
+                openai_api_key=""
             )
             response=llm([HumanMessage(content=request.query)])
             return {"respnse":response.content}
